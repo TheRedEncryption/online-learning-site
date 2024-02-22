0af90fc34138eef28a954b3f9b7d37cd48701ce8
Isaac Mills <rooster0055@protonmail.com>
2024-02-22 08:23:28 -0500

    Add the correct stack to project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
+use tracing_subscriber::EnvFilter;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
     println!("Hello, world!");
 }
