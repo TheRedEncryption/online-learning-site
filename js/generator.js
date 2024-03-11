ea4fcc6f242d28d89cb9eb9b28955823efbcba5a
TheRedEncryption <64206126+TheRedEncryption@users.noreply.github.com>
2024-03-11 12:58:31 +0000

    javascript

---
+++ js/generator.js
@@ -0,0 +1,15 @@
+// The purpose of this script is to be attached to websites and then generate DOM elements based on existing classes
+
+document.addEventListener('DOMContentLoaded', initialize, false);
+
+function initialize() {
+    // init stuff goes here
+    window.alert("Yeah!");
+}
+
+/*
+add functions here for generating:
+ - header div
+ - footer div
+ - other divs that require multiple child DOMs
+*/
