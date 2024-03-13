3effa020240eff8d6bdfe8c03413ecb60390cae1
TheRedEncryption <64206126+TheRedEncryption@users.noreply.github.com>
2024-03-13 15:02:47 +0000

    big gains

--- js/generator.js
+++ js/generator.js
@@ -15,5 +15,17 @@ add functions here for generating:
 */
 
 function populateHeader() {
-    
+    /*
+    how this function is supposed to work:
+    1. this function finds an empty div with class "header"
+    2. if found, set the inner HTML to the constant that is provided above (will add later)
+    3. depending on the page, make one of the buttons depressed :(
+        (for example, on "Learn" page, make the "learn" button gray instead of white)
+    */
+}
+
+// data functions (aka, functions specifically for custom attributes, those that start with "data-")
+
+function imagehref() {
+    // TODO: get table of all elements with data-imagehref and then set their background images to the image
 }
