5443c0cb4e67e3d8263553ea8716e9f8ee3dd6f6
TheRedEncryption <64206126+TheRedEncryption@users.noreply.github.com>
2024-03-22 12:41:28 +0000

    added a bunch of periods, hoping this works

--- js/generator.js
+++ js/generator.js
@@ -48,14 +48,14 @@ function replaceTemplates(){
 function favicon(){
     let headicons =
     `
-    <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png">
-    <link rel="manifest" href="/icons/site.webmanifest">
-    <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#b91d47">
-    <link rel="shortcut icon" href="/icons/favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="./icons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./icons/favicon-16x16.png">
+    <link rel="manifest" href="./icons/site.webmanifest">
+    <link rel="mask-icon" href="./icons/safari-pinned-tab.svg" color="#b91d47">
+    <link rel="shortcut icon" href="./icons/favicon.ico">
     <meta name="msapplication-TileColor" content="#b91d47">
-    <meta name="msapplication-config" content="/icons/browserconfig.xml">
+    <meta name="msapplication-config" content="./icons/browserconfig.xml">
     <meta name="theme-color" content="#000000">
     `
     $('head').append(headicons);
