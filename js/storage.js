8c0d6fb13796910e0eb9e10ba2ab4571eaed702f
Jamieson-H7 <114550501+Jamieson-H7@users.noreply.github.com>
2024-04-09 13:05:06 +0000

    Storage shuff

--- js/storage.js
+++ js/storage.js
@@ -37,30 +37,49 @@ function initialCall() {
   uploadFileButton.addEventListener("click", () => {
     console.log("%cYIPPEE!", "color:rgb(0,100,100); font-size:30px; background-color:rgb(0,60,240)");
     console.log(uploadFileInput.files[0].name);
+
+
+    let projectName = document.getElementById("projectName");
+    if(projectName==""){
+      return console.error("No project name")
+    }
     // 'file' comes from the Blob or File API
     //return;
-    if (!uploadFileInput.files.length || !storageRef) {
-      return
+    // if ((!uploadFileInput.files.length || !storageRef) && (!document.getElementById("textarea").value=="")) {
+    //   return console.error("No file/textarea value")
+    // }
+    var blob = new Blob([document.getElementById("textarea").value], {type: "text/plain"});
+    /*uploadFileInput.files[0].name*/
+    if(uploadFileInput.files[0]){
+      uploadBytes(ref(storageRef, projectName.value + "/" + uploadFileInput.files[0].name), uploadFileInput.files[0]).then((snapshot) => {
+        console.log('Uploaded a blob or file!');
+        document.getElementById("toast-content").innerText = "Successfully uploaded file";
+        document.getElementById("toast-header-text").innerText = "File Uploaded";
+        var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+        var toastList = toastElList.map(function (toastEl) {
+          return new bootstrap.Toast(toastEl)
+        })
+        toastList.forEach(toast => toast.show())
+      }).catch((error) => {
+        console.error(error);
+        if(error.toString().includes("User does not have permission to access")){
+          document.getElementById("toast-content").innerText = "File does not meet requirements for uploading. \n\n" + error;
+        }
+        document.getElementById("toast-header-text").innerText = "File Upload Issue";
+        var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+        var toastList = toastElList.map(function (toastEl) {
+          return new bootstrap.Toast(toastEl)
+        })
+        toastList.forEach(toast => toast.show())
+      })
     }
-    uploadBytes(ref(storageRef, uploadFileInput.files[0].name), uploadFileInput.files[0]).then((snapshot) => {
-      console.log('Uploaded a blob or file!');
-      document.getElementById("toast-content").innerText = "Successfully uploaded file";
-      document.getElementById("toast-header-text").innerText = "File Uploaded";
-      var toastElList = [].slice.call(document.querySelectorAll('.toast'))
-      var toastList = toastElList.map(function (toastEl) {
-        return new bootstrap.Toast(toastEl)
+    if(blob){
+      uploadBytes(ref(storageRef, projectName.value + "/" + "markdownText.txt"), blob).then((snapshot) => {
+        console.log('Uploaded the markdown blob or file!');
+      }).catch((error) => {
+        console.error(error);
       })
-      toastList.forEach(toast => toast.show())
-    }).catch((error) => {
-      if(error.toString().includes("User does not have permission to access")){
-        document.getElementById("toast-content").innerText = "File does not meet requirements for uploading. \n\n" + error;
-      }
-      document.getElementById("toast-header-text").innerText = "File Upload Issue";
-      var toastElList = [].slice.call(document.querySelectorAll('.toast'))
-      var toastList = toastElList.map(function (toastEl) {
-        return new bootstrap.Toast(toastEl)
-      })
-      toastList.forEach(toast => toast.show())
-    })
+    }    
+
   })
 }
