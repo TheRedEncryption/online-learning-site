7504076e9cb98196662c24ba9b443b5aea5aa9e4
Jamieson-H7 <114550501+Jamieson-H7@users.noreply.github.com>
2024-04-24 15:24:31 +0000

    IT IS SORTED YES

--- js/databased.js
+++ js/databased.js
@@ -13,10 +13,10 @@ onAuthStateChanged(auth, (user) => {
     if (user) {
         // User is signed in, see docs for a list of available properties
         uid = user.uid;
-        if(user.displayName){
+        if (user.displayName) {
             username = user.displayName
         }
-        else{
+        else {
             username = user.email;
         }
         // email = user.email
@@ -49,19 +49,42 @@ window.addEventListener("load", () => {
     const messagesRef = ref(db, 'messages/');
     onValue(messagesRef, (snapshot) => {
         const data = snapshot.val();
-        centerBody.innerHTML=""
+        centerBody.innerHTML = ""
+        let messagesByTimestamp = [];
         for (const [key, value] of Object.entries(data)) {
             console.log(`key1 ${key}: ${value}`);
             for (const [key2, value2] of Object.entries(value)) {
                 console.log(`key2 ${key2}: ${value2}`);
+                messagesByTimestamp.push({
+                    timestamp: key2,
+                    value: value2
+                })
                 console.log(`MESSAGE ${value2.message}`);
-                centerBody.innerHTML += `<div><b>${value2.username}</b><p>${value2.message}</p></div>`
-                // for (const [key3, value3] of Object.entries(value2)) {
-                //     console.log(`key3 ${key3}: ${value3}`);
-
-                // }
+                // centerBody.innerHTML += `<div><b>${value2.username}</b><p>${value2.message}</p></div>`
             }
         }
+        messagesByTimestamp.sort(function (a, b) {
+            var keyA = a.timestamp,
+                keyB = b.timestamp;
+            // Compare the 2 dates
+            if (keyA < keyB) return -1;
+            if (keyA > keyB) return 1;
+            return 0;
+        });
+        console.log(messagesByTimestamp)
+        for(var i = 0; i < messagesByTimestamp.length; i++){
+            let thinginarray = messagesByTimestamp[i];
+            centerBody.innerHTML += `<div><b>${thinginarray.value.username}</b><p>${thinginarray.value.message}</p></div>`
+        }
+        // messagesByTimestamp.forEach((thinginarray)=>{
+        //     centerBody.innerHTML += `<div><b>${thinginarray.value.username}</b><p>${thinginarray.value.message}</p></div>`
+        // })
+        // for(var element of messagesByTimestamp){
+        //     // value2 = element
+        //     console.log(element);
+        //     break;
+        //     centerBody.innerHTML += `<div><b>${element.value.username}</b><p>${element.value.message}</p></div>`
+        // }
         console.log(data);
         console.log(typeof (data));
     });
