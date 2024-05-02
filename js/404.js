537960f1c563c54a3a322d860e3be988dfb5308a
Jamieson-H7 <114550501+Jamieson-H7@users.noreply.github.com>
2024-05-02 12:55:38 +0000

    soryyy *hits you with metal pipe* sorgy

--- js/404.js
+++ js/404.js
@@ -115,7 +115,7 @@ else if(urlArray[0] === 'course'){
                     console.log("userIDS")
                     console.log(userIDS)
                     userIDS.prefixes.forEach((articleRef) => {
-                        document.body.innerHTML += articleModuleBuilder.buildCourse(articleRef.name, articleRef.parent.name);
+                        document.getElementById("markdownHolder").innerHTML += articleModuleBuilder.buildCourse(articleRef.name, articleRef.parent.name);
                     });
                 })
             });
