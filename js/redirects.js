11f0d4e86fd9527bebc8c8ba8d53e06dc9bc02af
TheRedEncryption <64206126+TheRedEncryption@users.noreply.github.com>
2024-05-02 11:42:37 +0000

    fixed header

--- js/redirects.js
+++ js/redirects.js
@@ -1,18 +1,24 @@
+// Redirects to index
+function redirectToIndex(){
+    safeRedirect("index.html");
+}
+
 // Redirects to a sign in page
 function redirectToSignIn(){
     safeRedirect("userAuth.html");
 }
 
-// Redirects to a sign in page
+// Redirects to learn page
 function redirectToLearnPage(){
     safeRedirect("learn.html");
 }
 
-// Redirects to a sign in page
+// Redirects to discuss page
 function redirectToDiscussPage(){
     safeRedirect("discuss.html");
 }
 
+// safely redirects based on whether or not you're using production or dev
 function safeRedirect(destination){
     if(window.location.origin == "https://theredencryption.github.io"){
         window.location.replace("https://theredencryption.github.io/online-learning-site/" + destination);
